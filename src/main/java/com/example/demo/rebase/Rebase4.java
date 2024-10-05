a8fa24b4ac769d93d50bed1dd5c00f34ab5d4366
anton <antontitow@bk.ru>
2024-10-06 01:36:58 +0300

    start

--- src/main/java/com/example/demo/rebase/Rebase4.java
+++ src/main/java/com/example/demo/rebase/Rebase4.java
@@ -1,10 +1,5 @@
 package com.example.demo.rebase;
 
 public class Rebase4 {
-    private String start1;
-    private String start2;
-    private String start3;
-    private String on1;
-    private String on2;
-    private String on3;
+
 }
