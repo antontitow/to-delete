dec5190f0a345a3442dbd9d63e3609f4e6b912e6
anton <antontitow@bk.ru>
2024-10-05 23:49:40 +0300

    start2 rrr

--- src/main/java/com/example/demo/rebase/Rebase3.java
+++ src/main/java/com/example/demo/rebase/Rebase3.java
@@ -2,6 +2,7 @@ package com.example.demo.rebase;
 
 public class Rebase3 {
     private String start1;
+    private String start2;
     private String one1;
     private String one2;
     private String two1;
