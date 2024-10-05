a9f1b595ffdace9bfdaee9a82c4f5b920650ece3
anton <antontitow@bk.ru>
2024-10-05 22:48:11 +0300

    master master 8 rebase rebase

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -36,6 +36,7 @@ public class DemoApplication {
         System.out.println("master6");
         System.out.println("master7");
         System.out.println("master8");
+        System.out.println("one 8");
         System.out.println("two2");
         System.out.println("two3");
         System.out.println("two4");
