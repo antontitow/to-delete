0cdd57ff5464081b8b4e20d0673ea078fab73af8
anton <antontitow@bk.ru>
2024-10-05 22:42:20 +0300

    master master 6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -23,6 +23,7 @@ public class DemoApplication {
         System.out.println("master3");
         System.out.println("master4");
         System.out.println("master5");
+        System.out.println("master6");
 
 //        val groupByWho = getHateDtos().stream().collect(groupingBy(HateDto::getWho));
 //        show(groupByWho);
