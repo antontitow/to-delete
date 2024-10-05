8692be85975c3e25c3c8dac93d10073520307d34
anton <antontitow@bk.ru>
2024-10-05 20:48:55 +0300

    rebase

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -19,6 +19,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("master");
+        System.out.println("one");
         System.out.println("master2");
         System.out.println("master3");
         System.out.println("master4");
