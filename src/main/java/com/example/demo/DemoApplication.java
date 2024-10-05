8f13a362d7ab8a518e791d4204e6687840f27b28
anton <antontitow@bk.ru>
2024-10-05 20:51:52 +0300

    two add two update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -19,6 +19,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("two");
+        System.out.println("two update");
         System.out.println("master");
         System.out.println("master2");
 
